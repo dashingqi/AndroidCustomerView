fe2920a59bc385587613b4ab1cfdd67e5bf1c263
dashingqi <1415942241@qq.com>
2019-07-25 18:04:18 +0800

    [增加工具类]

--- 02_viewpager/src/main/java/wanandroidqi/dashingqi/com/a02_viewpager/DensityUtil.java
+++ 02_viewpager/src/main/java/wanandroidqi/dashingqi/com/a02_viewpager/DensityUtil.java
@@ -4,10 +4,27 @@ import android.content.Context;
 
 public class DensityUtil {
 
+    /**
+     * dp 转 px
+     *
+     * @param context
+     * @param dpValue
+     * @return
+     */
     public static int dp2px(Context context, float dpValue) {
-        //获取到像素密度
+        //获取到设备的像素密度
         float density = context.getResources().getDisplayMetrics().density;
         return (int) (dpValue * density + 0.5f);
+    }
 
+    /**
+     * sp 转 px
+     * @param context
+     * @param spValue
+     * @return
+     */
+    public static int sp2px(Context context, float spValue) {
+        float scaledDensity = context.getResources().getDisplayMetrics().scaledDensity; //获取到缩放密度
+        return (int) (spValue * scaledDensity + 0.5f);
     }
 }
